2ba1f35d653b50bd17097698978d43b977c0c457
Kevin Seiden <alshain01@suddenlink.net>
2013-11-05 15:44:47 -0600

    Cleanup and optimization

--- FlagsCreatureSpawn/src/alshain01/FlagsCreatureSpawn/FlagsCreatureSpawn.java
+++ FlagsCreatureSpawn/src/alshain01/FlagsCreatureSpawn/FlagsCreatureSpawn.java
@@ -20,9 +20,10 @@
 
  Notice — For any reuse or distribution, you must make clear to others the license terms of this work. The best way to do this is with a link to this web page.
  http://creativecommons.org/licenses/by-nc/3.0/
-*/
+ */
 
 package alshain01.FlagsCreatureSpawn;
+
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.EntityType;
@@ -33,111 +34,135 @@ import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import alshain01.Flags.Director;
+import alshain01.Flags.Flag;
 import alshain01.Flags.Flags;
 import alshain01.Flags.ModuleYML;
-import alshain01.Flags.Flag;
 import alshain01.Flags.Registrar;
-import alshain01.Flags.Director;
 
 /**
- * Flags - Creature Spawn
- * Module that adds creature spawn flags to the plugin Flags.
+ * Flags - Creature Spawn Module that adds creature spawn flags to the plugin Flags.
  * 
  * @author Alshain01
  */
 public class FlagsCreatureSpawn extends JavaPlugin {
+	/*
+	 * The event handler for the flags we created earlier
+	 */
+	private class CreatureSpawnListener implements Listener {
+		@EventHandler(ignoreCancelled = true)
+		private void onCreatureSpawn(CreatureSpawnEvent e) {
+			Flag flag = null;
+			final Registrar flags = Flags.getRegistrar();
+
+			switch (e.getSpawnReason()) {
+			case NATURAL:
+				flag = flags.getFlag("SpawnMob");
+				break;
+			case VILLAGE_INVASION:
+				flag = flags.getFlag("SpawnInvasion");
+				break;
+			case EGG:
+				flag = flags.getFlag("SpawnEgg");
+				break;
+			case JOCKEY:
+				flag = flags.getFlag("SpawnJockey");
+				break;
+			case LIGHTNING:
+				flag = flags.getFlag("SpawnLightning");
+				break;
+			case VILLAGE_DEFENSE:
+				flag = flags.getFlag("SpawnGolem");
+				break;
+			case CUSTOM:
+				flag = flags.getFlag("SpawnByPlugin");
+				break;
+			case CHUNK_GEN:
+				flag = flags.getFlag("SpawnChunk");
+				break;
+			case SPAWNER:
+				flag = flags.getFlag("Spawner");
+				break;
+			case SPAWNER_EGG:
+				flag = flags.getFlag("SpawnerEgg");
+				break;
+			case BUILD_IRONGOLEM:
+				flag = flags.getFlag("BuildGolem");
+				break;
+			case BUILD_SNOWMAN:
+				flag = flags.getFlag("BuildSnowman");
+				break;
+			case BREEDING:
+				if (e.getEntityType() == EntityType.VILLAGER) {
+					flag = flags.getFlag("BreedVillager");
+				}
+				break;
+			default:
+				// Can't switch on API versions, will cause errors.
+				if (Flags.checkAPI("1.2.5")
+						&& e.getSpawnReason() == SpawnReason.SLIME_SPLIT) {
+					flag = flags.getFlag("SlimeSplit");
+				} else if (Flags.checkAPI("1.4.5")
+						&& e.getSpawnReason() == SpawnReason.BUILD_WITHER) {
+					flag = flags.getFlag("BuildWither");
+				} else if (Flags.checkAPI("1.6.2")
+						&& e.getSpawnReason() == SpawnReason.REINFORCEMENTS) {
+					flag = flags.getFlag("SpawnReinforcements");
+				} else {
+					flag = flags.getFlag("SpawnOther");
+				}
+				break;
+			}
+
+			// Always guard this, even when it really can't happen.
+			// (In this case, BREEDING can cause null)
+			if (flag != null) {
+				e.setCancelled(!Director.getAreaAt(e.getLocation()).getValue(
+						flag, false));
+			}
+		}
+	}
+
 	/**
 	 * Called when this module is enabled
 	 */
 	@Override
-	public void onEnable(){
-		PluginManager pm =  Bukkit.getServer().getPluginManager();
+	public void onEnable() {
+		final PluginManager pm = Bukkit.getServer().getPluginManager();
 
-		if(!pm.isPluginEnabled("Flags")) {
-		    this.getLogger().severe("Flags was not found. Shutting down.");
-		    pm.disablePlugin(this);
+		if (!pm.isPluginEnabled("Flags")) {
+			getLogger().severe("Flags was not found. Shutting down.");
+			pm.disablePlugin(this);
 		}
-		
+
 		// Connect to the data file
-		ModuleYML dataFile = new ModuleYML(this, "flags.yml");
-		
+		final ModuleYML dataFile = new ModuleYML(this, "flags.yml");
+
 		// Register with Flags
-		Registrar flags = Flags.getRegistrar();
-		for(String f : dataFile.getModuleData().getConfigurationSection("Flag").getKeys(false)) {
-			ConfigurationSection data = dataFile.getModuleData().getConfigurationSection("Flag." + f);
-			
+		final Registrar flags = Flags.getRegistrar();
+		for (final String f : dataFile.getModuleData().getConfigurationSection("Flag").getKeys(false)) {
+			final ConfigurationSection data =
+					dataFile.getModuleData().getConfigurationSection("Flag." + f);
+
 			// We don't want to register flags that aren't supported.
 			// It would just muck up the help menu.
 			// Null value is assumed to support all versions.
-			String api = data.getString("MinimumAPI");  
-			if(api != null && !Flags.checkAPI(api)) { continue; }
-			
+			final String api = data.getString("MinimumAPI");
+			if (api != null && !Flags.checkAPI(api)) {
+				continue;
+			}
+
 			// The description that appears when using help commands.
-			String desc = data.getString("Description");
-			
-			// Register it!  (All flags are defaulting to true in this module)
+			final String desc = data.getString("Description");
+
+			// Register it! (All flags are defaulting to true in this module)
 			// Be sure to send a plug-in name for the help command!
 			// It can be this.getName() or another string.
 			flags.register(f, desc, true, "CreatureSpawn");
 		}
-		
+
 		// Load plug-in events and data
-		Bukkit.getServer().getPluginManager().registerEvents(new CreatureSpawnListener(), this);
-	}
-	
-	/*
-	 * The event handler for the flags we created earlier
-	 */
-	private class CreatureSpawnListener implements Listener{
-		@EventHandler(ignoreCancelled = true)
-		private void onCreatureSpawn(CreatureSpawnEvent e){
-			Flag flag = null;
-			Registrar flags = Flags.getRegistrar();
-			
-			if (e.getSpawnReason() == SpawnReason.NATURAL) {
-				flag = flags.getFlag("SpawnMob");
-			} else if (e.getSpawnReason() == SpawnReason.VILLAGE_INVASION) {
-				flag = flags.getFlag("SpawnInvasion");
-			} else if (e.getSpawnReason() == SpawnReason.EGG) {
-				flag = flags.getFlag("SpawnEgg");
-			} else if (e.getSpawnReason() == SpawnReason.JOCKEY) {
-				flag = flags.getFlag("SpawnJockey");
-			} else if (e.getSpawnReason() == SpawnReason.LIGHTNING) {
-				flag = flags.getFlag("SpawnLightning");
-			} else if (e.getSpawnReason() == SpawnReason.VILLAGE_DEFENSE) {
-				flag = flags.getFlag("SpawnGolem");
-			} else if (e.getSpawnReason() == SpawnReason.CUSTOM) {
-				flag = flags.getFlag("SpawnByPlugin");
-			} else if (e.getSpawnReason() == SpawnReason.CHUNK_GEN) {
-				flag = flags.getFlag("SpawnChunk");
-			} else if (e.getSpawnReason() == SpawnReason.SPAWNER) {
-				flag = flags.getFlag("Spawner");
-			} else if (e.getSpawnReason() == SpawnReason.SPAWNER_EGG) {
-				flag = flags.getFlag("SpawnerEgg");
-			} else if (e.getSpawnReason() == SpawnReason.BUILD_IRONGOLEM) {
-				flag = flags.getFlag("BuildGolem");
-			} else if (e.getSpawnReason() == SpawnReason.BUILD_SNOWMAN) {
-				flag = flags.getFlag("BuildSnowman");
-			} else if (e.getSpawnReason() == SpawnReason.BREEDING) {
-				if (e.getEntityType() == EntityType.VILLAGER) {
-					flag = flags.getFlag("BreedVillager");
-				}
-			} else if (Flags.checkAPI("1.2.5")
-					&& e.getSpawnReason() == SpawnReason.SLIME_SPLIT) {
-				flag = flags.getFlag("SlimeSplit");
-			} else if(Flags.checkAPI("1.4.5")
-					&& e.getSpawnReason() == SpawnReason.BUILD_WITHER) {
-				flag = flags.getFlag("BuildWither");
-			} else if(Flags.checkAPI("1.6.2")
-					&& e.getSpawnReason() == SpawnReason.REINFORCEMENTS) {
-				flag = flags.getFlag("SpawnReinforcements");
-			} else {
-				flag = flags.getFlag("SpawnOther");
-			}
-			
-			if (flag != null) { // Always guard this, even when it really can't happen. (In this case, BREEDING can cause null)
-				e.setCancelled(!Director.getAreaAt(e.getLocation()).getValue(flag, false));
-			}
-		}
+		Bukkit.getServer().getPluginManager()
+				.registerEvents(new CreatureSpawnListener(), this);
 	}
 }
